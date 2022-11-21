49ca5afdb07ac2d078fb0c7a25fb41b2ff7860ef
Ardenis9 <ardenis.nikolli@gmail.com>
2022-11-21 12:52:04 -0500

    Sixth commit,but it will be first in the new branch

--- src/Sum_of_Nums.java
+++ src/Sum_of_Nums.java
@@ -2,14 +2,14 @@ public class Sum_of_Nums {
         public static void main(String[] args) {
             // two integer variables with values
             // and a variable "sum" to store the result
-            int num1 = 5, num2 = 17,sum; // changed the num2
+            int num1 = 5, num2 = 17, num3 = 12, sum; // changed the num2
 
             //calculating the sum of num1 and num2 and
             //storing the result in the variable sum
-            sum = num1+num2;
+            sum = num1+num2+num3;
 
             //printing the result
-            System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+            System.out.println(sum);
         }
     }
 
