04517b702e716045fa79155b84f36d6f583180cf
Ardenis9 <ardenis.nikolli@gmail.com>
2022-11-21 11:47:54 -0500

    my third commit changed number

--- src/Sum_of_Nums.java
+++ src/Sum_of_Nums.java
@@ -2,7 +2,7 @@ public class Sum_of_Nums {
         public static void main(String[] args) {
             // two integer variables with values
             // and a variable "sum" to store the result
-            int num1 = 5, num2 = 15,sum;
+            int num1 = 5, num2 = 16,sum; // changed the num2
 
             //calculating the sum of num1 and num2 and
             //storing the result in the variable sum
